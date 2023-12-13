60b74c3791f0f77993a14929efac9d179f58b2c0
Aarsh Mehta <113206841+aarsh-mehta@users.noreply.github.com>
2023-12-13 16:10:39 -0500

    dashboard for driver completed

--- static/userAsDriverTable.js
+++ static/userAsDriverTable.js
@@ -16,4 +16,43 @@ function createTripTable(trips, title, elementID) {
         table += "</tr>";
         table += "</thead>";
         
-  
+        for (let i = trips.length-1; i >= 0; i--) {
+
+            table += "<tbody>";
+            table += "<tr>";
+            table += "<td>" + trips[i]["dateOfTrip"]+"</td>";
+            table += "<td>" + trips[i]["time"]+"</td>";
+            table += "<td>" + trips[i]["origin"]+"</td>";
+            table += "<td>" + trips[i]["destination"]+"</td>";
+
+            passengerList = trips[i]["passengers"];
+
+            if (passengerList.length === 0) {
+                table +=  "<td> No passengers ~</td>";
+            } else {
+                table += "<td>"
+                for (let j = 0; j < passengerList.length; j++) {
+                    let passengerFirstName = passengerList[j]["userFirstName"]
+                    let passengerProfileImg = passengerList[j]["userProfileImg"]
+                    let passengerBio = passengerList[j]["userBio"]
+
+                    table += "<div class='row'>"
+                                    + "<div class='col-sm'>"
+                                    + `<img src=${passengerProfileImg} class="passenger-img rounded-circle" id="popover" data-toggle="popover" data-placement="left" tabindex="0" data-trigger="hover" data-content="<strong>${passengerFirstName}</strong>  ${passengerBio}">`
+                                    + "<br>"
+                                    + '<button type="button" data-placement="left" class="badge btn-custom" data-toggle="modal" data-target="#exampleModalCenter">Message</button>'
+                                    + "</div>"
+                                    + "</div>"
+                                    + "<br>"
+                } 
+                table += "</td>"
+            }
+            table += "</tr>";
+        }  
+        table += "</tbody>"
+        table += "</table>";
+        document.getElementById(elementID).innerHTML = table;
+        
+    }
+
+}
