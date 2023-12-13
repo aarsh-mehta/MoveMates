3eefa5f0d282a9ae1dde795a2e860290bf6a1e63
Aarsh Mehta <113206841+aarsh-mehta@users.noreply.github.com>
2023-12-13 15:47:20 -0500

    added messanging funtion api

--- server.py
+++ server.py
@@ -16,3 +16,19 @@ app = Flask(__name__)
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
+
+googlePlaceKey = os.environ['GOOGLE_PLACES_KEY']
+twilioSID = os.environ['TWILIO_SID']
+twilioAuthKey = os.environ['TWILIO_AUTH_KEY']
+twilioNum = os.environ['TWILIO_NUM']
+myNum = os.environ['MY_NUM']
+
+# Normally, if you use an undefined variable in Jinja2, it fails
+# silently. This is horrible. Fix this so that, instead, it raises an
+# error.
+app.jinja_env.undefined = StrictUndefined
+
+# Flask Upload
+UPLOAD_FOLDER = '/static/img'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg','webp'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
